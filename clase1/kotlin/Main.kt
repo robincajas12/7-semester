32f664f336b5c233709c4ef7b73d192b893ecff3
Robinson Cajas <75333543+robincajas12@users.noreply.github.com>
2026-04-13 10:34:28 -0500

    Enhance main2 function with various Kotlin features

--- clase1/kotlin/Main.kt
+++ clase1/kotlin/Main.kt
@@ -53,3 +53,107 @@ fun Int.hello()
 {
     println(this * 5)
 }
+
+
+import kotlin.random.Random
+import kotlin.random.Random.Default.nextInt
+
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+fun main2() {
+    val xd = xd();
+    println(xd?.let { "Hello, there's data here" });
+    println("Hello, world!!!" + a("a"){
+        println("#")
+    }.b())
+    if(5 == 5) println(45)
+
+    val xd1 = when(4){
+        5 -> "lol"
+        else -> "7w7"
+    }
+    println(xd1)
+    print(a("a"){
+        println("#789456")
+    }.apply{
+        b();
+    })
+
+    for(i in 8 downTo 1)
+    {
+        println(i)
+    }
+
+    for (i in 8 downTo 2 step 2){
+        println(i)
+    }
+
+    alsoFunction()
+
+    val a : Array<String> = arrayOf("a", "b", "c");
+
+    val objetoA = a("a"){
+        println("#789456")
+    }
+
+    with(objetoA){
+        println(this)
+        b()
+    }
+
+    xd(5){
+        println(this)
+        println("dentro de a")
+    }
+    val arr = Array(5, {i -> (i * i).toString()})
+    val arr2 = Array(5, {it})
+    println(nextInt(1,50))
+    println(arr.joinToString())
+    println(arr2.joinToString())
+
+    for(i in arr.indices)
+    {
+        println(arr[i])
+    }
+
+    for(i in arr.indices)
+    {
+        arr[i] = readLine()!!.toInt().also{println("numero:" + it + "ingresado" ) }.toString()
+    }
+    println(arr.joinToString())
+}
+
+
+fun xd(xdss: Int, ext : a.()-> Unit) :String
+{
+    println(xdss)
+    println(a("unu", {print("unu")}).ext())
+    return "xd"
+}
+fun xd() : String? {
+    return "5645646";
+}
+data class a(val ax : String,val lamda : ()-> Unit)
+{
+    fun b()
+    {
+        lamda()
+    }
+}
+
+fun alsoFunction() {
+    val medals: List<String> = listOf("Gold", "Silver", "Bronze")
+    val reversedLongUppercaseMedals: List<String> =
+        medals
+            .map { it.uppercase() }
+            .also { println(it) }
+            // [GOLD, SILVER, BRONZE]
+            .filter { it.length > 4 }
+            .also { println(it) }
+            // [SILVER, BRONZE]
+            .reversed()
+    println(reversedLongUppercaseMedals)
+    // [BRONZE, SILVER]
+}
