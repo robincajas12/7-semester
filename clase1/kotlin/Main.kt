6c1247b07ff6cc652627d64bd544c4afbce2a6ee
Robinson Cajas <75333543+robincajas12@users.noreply.github.com>
2026-04-15 11:06:06 -0500

    Update Main.kt

--- clase1/kotlin/Main.kt
+++ clase1/kotlin/Main.kt
@@ -6,6 +6,14 @@ import kotlin.time.Duration.Companion.seconds
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 suspend fun main() {
+val x = 2
+
+when (x) {
+    1 -> println("Uno")
+    2 -> println("Dos")
+    3 -> println("Tres")
+    else -> println("Otro")
+}
 for (i in 1..5) {
     println(i)
 }
