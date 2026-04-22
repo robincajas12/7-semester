91dbe920e08c58e907aa3283d82efa15239e4e20
Robinson Cajas <75333543+robincajas12@users.noreply.github.com>
2026-04-22 12:33:12 -0500

    Update Main.kt

--- clase1/kotlin/Main.kt
+++ clase1/kotlin/Main.kt
@@ -180,3 +180,26 @@ fun main14052026(){
     print(matriz3.contentToString())
     
 }
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main22042026() {
+    val name = "Kotlin"
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    println("Hello, " + name + "!")
+
+    for (i in 1..5) {
+        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        println("i = $i")
+    }
+
+    val xd = mutableListOf("juan","pedro", "nia")
+    //xd.remove("juan")
+    xd.contains("pedro")
+    xd.indexOf("juan")
+    println(xd.sortDescending())
+    println(xd.sortedDescending())
+}
